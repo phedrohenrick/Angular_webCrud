b10ffb5b381a81d6cc072d01111902d42c9b638e
phedrohenrick <phedrohenrickdasilva@gmail.com>
2024-06-13 14:48:11 -0300

    configuração de rota

--- src/app/produtos/produtos.module.ts
+++ src/app/produtos/produtos.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { ProdutosRoutingModule } from './produtos-routing.module';
 import { ProdutosComponent } from './produtos/produtos.component';
 
@@ -11,7 +12,9 @@ import { ProdutosComponent } from './produtos/produtos.component';
   ],
   imports: [
     CommonModule,
-    ProdutosRoutingModule
+    ProdutosRoutingModule,
+    AppMaterialModule
+   
   ]
 })
 export class ProdutosModule { }
