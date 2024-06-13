9355766fb175fb0baeac462eeeec4b63027c8f68
phedrohenrick <phedrohenrickdasilva@gmail.com>
2024-06-13 14:50:45 -0300

    configuração de component

--- src/app/produtos/produtos/produtos.component.ts
+++ src/app/produtos/produtos/produtos.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Produtos } from '../model/produtos';
+import { ProdutosService } from '../services/produtos.service';
 
 @Component({
   selector: 'app-produtos',
   templateUrl: './produtos.component.html',
   styleUrl: './produtos.component.scss'
 })
-export class ProdutosComponent {
+
+export class ProdutosComponent implements OnInit {
+
+  produtos: Produtos[] = [
+    
+  ] ;
+  displayedColumns = ['_id','name','price_in_cents','ativo'];
+
+produtosService : ProdutosService;
+
+  constructor(){
+    this.produtosService = new ProdutosService();
+    this.produtos = this.produtosService.list();
+  }
+
+  ngOnInit(): void {
+      
+  }
 
 }
