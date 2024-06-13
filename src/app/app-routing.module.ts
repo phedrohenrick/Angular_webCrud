73189b2dec8d6e45a5e15c86667b20317364c596
phedrohenrick <phedrohenrickdasilva@gmail.com>
2024-06-13 14:47:35 -0300

    configuração de rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProdutosComponent } from './produtos/produtos/produtos.component';
+import path from 'path';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', pathMatch :'full', redirectTo: 'produtos' },
+  {
+    path: 'produtos',
+    loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule)
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
